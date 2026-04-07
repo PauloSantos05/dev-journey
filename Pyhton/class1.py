d53ee4102ed9fbd969cdd374a25f2950b92c3e95
PauloSantos05 <pauloprogramer123@outlook.com>
2026-04-07 17:08:57 -0300

    Initial commit

---
+++ Pyhton/class1.py
@@ -0,0 +1,10 @@
+class Example:
+    def __init__(self, name):
+        self.name = name
+    
+    def greet(self):
+        return f"Hello, {self.name}!"
+
+if __name__ == "__main__":
+    obj = Example("World")
+    print(obj.greet()) 
